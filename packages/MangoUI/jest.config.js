cf841c6714524791ab7ae4c5da71b3187e42c004
wong <47519741+huangyuan3h@users.noreply.github.com>
2020-05-09 14:28:18 +0800

    Merge pull request #8 from MangoYellowH/action-added
    add workflow

--- packages/MangoUI/jest.config.js
+++ packages/MangoUI/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     '.(ts|tsx)': 'ts-jest',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', 'dist'],
   testRegex: '(\\.(test|spec))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   setupFilesAfterEnv: ['./test/jest.setup.ts'],
