8175be27e4a9fbf27295846267aaf00ef20de477
huangyuan3h <huangyuan3h@gmail.com>
2020-05-12 08:06:28 +0800

    add eslint config

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', 'tsx', 'ts'] },
+    ],
+    'import/extensions': [
+      'error',
+      { extensions: ['.js', '.jsx', 'tsx', 'ts'] },
+    ],
+  },
+};
